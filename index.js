cb01fdfe5751f1e550b2dfa8d9d81d15e14d06f1
Jahirul83 <jahirulislamcse83@gmail.com>
2024-08-27 15:26:59 +0600

    data extracted from server

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const businessesCollection = client.db("bdHandicraftsDb").collection("businesses");
+
+    app.get('/businesses', async (req, res) => {
+        const result = await businessesCollection.find().toArray();
+        res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
